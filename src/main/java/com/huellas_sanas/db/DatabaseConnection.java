cb964ddaa4440f4fa66e11a8450e67d939f45bbf
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-04-23 20:46:53 -0500

    feat(DatabaseConnection): add getter to connection to database

--- src/main/java/com/huellas_sanas/db/DatabaseConnection.java
+++ src/main/java/com/huellas_sanas/db/DatabaseConnection.java
@@ -1,4 +1,59 @@
 package com.huellas_sanas.db;
 
+import com.huellas_sanas.config.AppConfig;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+
+    private static volatile DatabaseConnection instance;
+    private static final AppConfig config = AppConfig.getInstance();
+    private static final ThreadLocal<Connection> threadLocal = new ThreadLocal<>();
+
+    private DatabaseConnection () {
+
+        try {
+
+            Class.forName(config.getDriver());
+
+        }catch (ClassNotFoundException err) {
+
+            throw new RuntimeException("don't found JDBC driver:", err);
+
+        }
+
+    }
+
+    public static DatabaseConnection getInstance() {
+
+        if (instance == null){
+
+            synchronized (DatabaseConnection.class){
+
+                if (instance == null) instance = new DatabaseConnection();
+
+            }
+
+        }
+
+        return instance;
+
+    }
+
+    public Connection getConnection () throws SQLException {
+
+        if (threadLocal.get() != null && !threadLocal.get().isClosed()){
+
+            return threadLocal.get();
+
+        }
+
+        Connection conn = DriverManager.getConnection(config.getDbUrl(), config.getUser(), config.getPass());
+        threadLocal.set(conn);
+        return conn;
+
+    }
+
 }
